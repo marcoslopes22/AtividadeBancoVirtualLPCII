2da8f4f8878c086f655cb9a667150512d2f3d0ce
Marcos Aurélio <aurliomarcos@gmail.com>
2022-09-18 12:40:44 -0300

    correcao de bugs:bug:

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,30 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /*
-===========================================================================================
-ETE PORTO DIGITAL, RECIFE, VII-IX-XXXXII;
+=========================================================================================================
+ETE PORTO DIGITAL, RECIFE, XIX-IX-XXXXII;
 DESENVOLVIMENTO DE SISTEMAS - MÓDULO II;
-LÓGICA E PENSAMENTO COMPUTACIONAL II - PROFESSOR WALDECK;
+LÓGICA E PENSAMENTO COMPUTACIONAL II - WALDECK;
 
-ATIVIDADE: BANCO VIRTUAL PORTINHO VERSÃO 1.0;
+ATIVIDADE 01: BANCO VIRTUAL PORTINHO;
 GRUPO: LUCAS VIKTOR, VITÓRIA NEVES, GUILHERME FREITAS, RAVEL CESAR, MARCOS AURÉLIO, ELVIS BRUNO;
-===========================================================================================
+=========================================================================================================
 */
 
 public class Main {
 	
 	static final float SALDOIN	= 150f;
 	static float saldo			= SALDOIN;
+	static float rendimento		= 1.14f;
+	static boolean adicionarRendimento = false;
 	static int escolha;
 
 	static StringBuilder extrato = new StringBuilder();
 	static DateTimeFormatter dataAtual = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 	
 	public static void main(String[] args) throws InterruptedException, IOException{
+
+		GerenciarRendimento();
 
 		System.out.print("\033[H\033[2J");
 		do {
@@ -50,6 +54,11 @@ public class Main {
 					break;
 
 				case 5:
+					VerRendimento();
+					LimparTela();
+					break;
+
+				case 6:
 					SairDoSistema();
 					break;
 
@@ -58,9 +67,10 @@ public class Main {
 					LimparTela();
 					break;
 			};
-		} while(escolha != 5);
+		} while(escolha != 6);
 	};
 
+	/* IMPRIMIR TELA */
 	public static int MostrarTela(){
 		int opcaoEscolhida;
 		Scanner scan = new Scanner(System.in);
@@ -70,7 +80,8 @@ public class Main {
 		System.out.println("2 - Sacar");
 		System.out.println("3 - Consultar saldo");
 		System.out.println("4 - Consultar extrato");
-		System.out.println("5 - Sair");
+		System.out.println("5 - Rendimento");
+		System.out.println("6 - Sair");
 		System.out.println("======================================");
 
 		System.out.print("Digite sua operação: ");
@@ -79,13 +90,15 @@ public class Main {
 		return opcaoEscolhida;
 	};
 
+	/* LIMPAR A TELA; */
 	public static void LimparTela(){
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Pressione ENTER para continuar...\n");
+		System.out.println("\nPressione ENTER para continuar...\n");
 		String limpar = scan.nextLine();
 		System.out.print("\033[H\033[2J");
 	};
 
+	/* DEPOSITAR; */
 	public static void Depositar() {
 		float deposito;
 		do {
@@ -99,9 +112,10 @@ public class Main {
 			} else {
 				System.out.print("Valor inválido. Por favor, tente novamente:\n");
 			};
-		} while(deposito < 0);
+		} while(deposito <= 0);
 	};
 
+	/* SACAR; */
 	public static void Sacar(){
 		float saque;
 		do {
@@ -119,22 +133,55 @@ public class Main {
 		} while(saque > saldo || saque <= 0);
 	};
 
+	/* CONSULTAR SALDO; */
 	public static void ConsultarSaldo(){
 		System.out.printf("O seu saldo é de R$ %s reais\n", saldo);
 	};
 
+	/* CONSULTAR HISTÓRICO */
 	public static void ConsultarExtrado(){
-		System.out.println("=====================================");
 		System.out.println("EXTRATO");
 		System.out.println("-------------------------------------");
-		System.out.println(extrato);
+		if(extrato.length() <= 0){
+			System.out.println("Sem históricos registrados.");
+		} else {
+			System.out.println(extrato);
+		};
 		System.out.println("=====================================");
-	}
+	};
 
+	/* VERIFICAR SE O VALOR GERADO COM O RENDIMENTO JÁ FOI ATRIBUIDO AO SALDO; */
+	public static void GerenciarRendimento(){
+		if(adicionarRendimento == false){
+			float saldoComRendimento = (rendimento/100)*saldo;
+			saldo = saldo + saldoComRendimento;
+			adicionarRendimento = true;
+		} else {
+			adicionarRendimento = true;
+		};
+	};
+
+	/* CONSULTAR O SALDO COM RENDIMENTO; */
+	public static void VerRendimento(){
+		if(adicionarRendimento){
+			System.out.println("Rendimento mensal: "+rendimento+"%");
+			if(adicionarRendimento){
+				System.out.println("Status: Atribuído!");
+			};
+			float rendimentoProximoMes = rendimento/100*saldo;
+			System.out.println("Previsão do próximo mês: +"+rendimentoProximoMes+" reais");
+			// System.out.printf("Saldo com rendimento: R$ %s reais\n", saldo);
+		} else {
+			GerenciarRendimento();
+		};
+	};
+
+	/* PARAR A EXECUÇÃO DO CÓDIGO */
 	public static void SairDoSistema(){
 		System.out.println("Saindo do sistema...");
 	};
 
+	/* TRATAMENTO PARA OPÇÕES INVÁLIDAS; */
 	public static void OpcaoInvalida(){
 		System.out.println("Opção Inválida! Tente novamente!");
 	};
